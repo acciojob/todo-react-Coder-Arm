81958b486968f519cd3fc868740fdf4867dd357b
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-28 14:21:50 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [task,setTask] = useState("");
+  const [taskArr,setTaskArr] = useState([]);
+  // console.log(taskArr)
+  function addTask(e){
+      e.preventDefault();
+      setTaskArr([...taskArr,task])
+      setTask("");
+  }
+
+  function deleteTask(id){
+    setTaskArr( taskArr.filter((item,idx) => idx !== id))
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+        <form>
+          <input type="text" value = {task} onChange={(e) => setTask(e.target.value)}/>
+          <button onClick={addTask}>Add Todo</button>
+        </form>
+        {taskArr.length> 0 && taskArr.map((item,idx) => {
+         return ( <div>
+           <span>{item}</span>
+            <button onClick={() => deleteTask(idx)}>Delete</button>
+         </div>
+          
+          )
+        })}
     </div>
   )
 }
